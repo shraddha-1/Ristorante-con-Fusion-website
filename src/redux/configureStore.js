540cfcb295b3fbc3cb4f6e62314c578f6f43d22a
shraddha-1 <shraddhadshine192@gmail.com>
2021-07-20 16:54:20 +0530

    confusion website

---
+++ src/redux/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore,combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+       combineReducers({
+           dishes:Dishes,
+           comments:Comments,
+           promotions:Promotions,
+           leaders:Leaders
+       })
+    );
+    return store;
+}
+
